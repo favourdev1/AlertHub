5163a6031a9ea8bfb2ff81363dab9ea24de4c8f4
Favourdev <favourapps17@gmail.com>
2023-08-20 09:34:54 +0100

    initial commit

---
+++ src/CustomAlert.js
@@ -0,0 +1,24 @@
+class AlertHub {
+    constructor(parentElement = "body") {
+        this.alertContainer = document.createElement('div')
+        this.alertContainer.className = "alert-hub-container"
+        document.querySelector(parentElement).appendChild(this.alertContainer);
+
+    }
+
+
+    showAlert(params) {
+        const { title, description, background, type = "info", timeout = 1000 } = params
+        const alertBox = document.createElement("div")
+            // Set the CSS alert type.
+        alertBox.className = `alert-hub ${type}`;
+        // Set the content of the alert box using the provided title and description.
+        alertBox.innerHTML = `<h2>${title}</h2><p>${description}</p>`;
+
+
+        this.alertContainer.appendChild(alertBox)
+        setTimeout(() => {
+            this.alertContainer.removeChild(alertBox)
+        }, timeout);
+    }
+}
